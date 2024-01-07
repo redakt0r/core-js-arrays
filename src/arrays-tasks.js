6d78d73d5109e5c371eb2af6bc763a3fa8d72e9e
Mikhail Koryakin <sdrr87@gmail.com>
2024-01-07 18:05:45 +0300

    feat: sort digit names by numeric order

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -561,8 +561,25 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const map = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  function namesSort(a, b) {
+    if (map[a] < map[b]) return -1;
+    if (map[a] > map[b]) return 1;
+    return 0;
+  }
+  return arr.sort(namesSort);
 }
 
 /**
