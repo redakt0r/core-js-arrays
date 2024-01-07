7cc9e90b6b639e17684fc622397df57d0ef340c8
Mikhail Koryakin <sdrr87@gmail.com>
2024-01-07 14:02:35 +0300

    feat: create chunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -337,8 +337,12 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize, result = []) {
+  if (arr.length === 0) {
+    return result;
+  }
+  result.push(arr.splice(0, chunkSize));
+  return createChunks(arr, chunkSize, result);
 }
 
 /**
